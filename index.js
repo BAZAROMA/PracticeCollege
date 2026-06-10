66539dd469413ad8573a8f0ea6501aa3a7e7ecd5
BAZAROMA <bazaromano@gmail.com>
2026-06-11 03:10:06 +0500

    Environment example file created, index file added

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const cors    = require('cors');
+const tasks   = require('./controllers/tasksController');
+
+const app  = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
